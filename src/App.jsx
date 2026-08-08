1b91d6c79af8554d4edd4deabbaa824ee520cb5b
iftikharalideveloper <iftikharali.developer@gmail.com>
2026-08-08 16:25:00 +0500

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Iftikhar Ali's Portfolio</h1>
+      <p>There will be my Blogs and Projects</p>
+    </div>
+  )
+}
+
+export default App;
